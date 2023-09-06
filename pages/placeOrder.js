70d83472f1c7290b5f14f2405da7e0581ff42f84
unknown <bb3583400@gmail.com>
2023-09-06 22:55:16 +0600

    placeOrder

--- pages/placeOrder.js
+++ pages/placeOrder.js
@@ -1,4 +1,5 @@
 import CheckOutWizard from '@/components/CheckOutWizard';
+import Image from "next/image";
 import Layout from '@/components/Layout';
 import { Store } from '@/utils/Store';
 import { getError } from '@/utils/error';
@@ -53,7 +54,7 @@ const PlaceOrderScreen = () => {
         router.push(`/order/${data._id}`);
     } catch (error) {
         setLoading(false);
-        toast.error(getError(err));
+        toast.error(getError(error));
     }
   }
     return (
@@ -66,18 +67,111 @@ const PlaceOrderScreen = () => {
             Cart is empty<Link href="/">Go Shipping</Link>
         </div>) : 
         (<div className='grid md:grid-cols-4 md:gap-5'>
-            <div className="overflow-x-auto md:col-span-2">
+            <div className="overflow-x-auto md:col-span-3">
                 <div className="card p-5">
-                    <h2 className="mb-2 text-lg">Shipping Address</h2>
-                    {shippingAddress.fullName},
-                    {shippingAddress.city}, {' '}
-                    {shippingAddress.postalCode}, {' '}
-                    {shippingAddress.country}
+                    <div className="">
+                        <h2 className="mb-2 text-lg">Shipping Address</h2>
+                        {shippingAddress.fullName},
+                        {shippingAddress.city}, {' '}
+                        {shippingAddress.postalCode}, {' '}
+                        {shippingAddress.country}
+                    </div>
+                    <div className="">
+                        <Link href='/shipping'>Edit</Link>
+                    </div>
                 </div>
-                <div className="">
-                    <Link href='/shipping'>Edit</Link>
+                <div className="card  p-5">
+                    <h2 className="mb-2 text-lg">Payment Method</h2>
+                 <div>{paymentMethod}</div>
+                    <div>
+                    <Link href="/payment">Edit</Link>
+                    </div>
                 </div>
+                <div className="card overflow-x-auto p-5">
+              <h2 className="mb-2 text-lg">Order Items</h2>
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">Item</th>
+                    <th className="    p-5 text-right">Quantity</th>
+                    <th className="  p-5 text-right">Price</th>
+                    <th className="p-5 text-right">Subtotal</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cartItems.map((item) => (
+                    <tr key={item._id} className="border-b">
+                      <td>
+                        <Link href={`/product/${item.slug}`} className="flex items-center">
+
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                            style={{
+                              maxWidth: "100%",
+                              height: "auto"
+                            }}></Image>
+                          {item.name}
+
+                        </Link>
+                      </td>
+                      <td className=" p-5 text-right">{item.quantity}</td>
+                      <td className="p-5 text-right">${item.price}</td>
+                      <td className="p-5 text-right">
+                        ${item.quantity * item.price}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <div>
+                <Link href="/cart">Edit</Link>
+              </div>
             </div>
+
+            </div>
+            <div>
+            <div className="card  p-5">
+              <h2 className="mb-2 text-lg">Order Summary</h2>
+              <ul>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Items</div>
+                    <div>${itemsPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Tax</div>
+                    <div>${taxPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Shipping</div>
+                    <div>${shippingPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Total</div>
+                    <div>${totalPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <button
+                    disabled={loading}
+                    onClick={placeOrderHandler}
+                    className="primary-button w-full"
+                  >
+                    {loading ? 'Loading...' : 'Place Order'}
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
 
         </div>)
     }

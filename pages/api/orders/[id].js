427373b5c224ccc5052a8459ad3a0f8847a8517b
unknown <bb3583400@gmail.com>
2023-09-09 23:57:57 +0600

    order

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -7,8 +7,12 @@ const { getSession } = require("next-auth/react")
 const handler = async (req, res) =>{
     const session = await getSession({ req});
     if(!session){
-        return res.status(401).send('signIn required')
+        return res.status(401).send('signIn required');
     }
     await db.connect()
     const order = await Order.findById(req.query.id);
+    await db.disconnect();
+    res.send(order);
 }
+
+export default handler;
---
+++ pages/order/[id].js
@@ -0,0 +1,15 @@
+import Layout from "@/components/Layout";
+
+
+
+function OrderScreen(){
+
+
+    return(
+        <Layout title={`Order ${orderId}`}>
+
+        </Layout>
+    )
+}
+OrderScreen.auth = true;
+export default OrderScreen;

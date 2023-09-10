ee4f2cc0eb98464a245f0166b3f06a1c7213b673
unknown <bb3583400@gmail.com>
2023-09-10 23:52:35 +0600

    oder

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,8 +1,43 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useReducer } from "react";
+
+
+
+function reducer(state, action){
+    switch (action.type){
+        case 'FETCH_REQUEST':
+            return {...state, loading: true, error: ''};
+        case 'FETCH_SUCCESS':
+            return{...state, loading: false, order: action.payload, error: ''};
+        case 'FETCH_FAIL':
+            return {...state, loading: false, error: action.payload};
+        default:
+            state;
+    }
+}
 
 
 
 function OrderScreen(){
+    const {query} = useRouter();
+    const orderId = query.id;
+    const [
+        {loading, error, order},
+        dispatch
+    ] = useReducer(reducer,{
+        loading: true,
+        order: {},
+        error: '',
+    });
+
+    useEffect(()=>{
+const fetchOrder = async () => {
+    dispatch({type: 'FETCH_REQUEST'});
+    const {data} = await axios.get(`/api/orders/${orderId}`)
+}
+    },[])
 
 
     return(
